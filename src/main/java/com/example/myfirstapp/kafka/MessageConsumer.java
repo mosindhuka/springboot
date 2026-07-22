0aec21d7afb3a49368099255336944caf8795419
M D <mosindhuka@gmail.com>
2026-07-22 23:55:16 +0530

    kafka retry

--- src/main/java/com/example/myfirstapp/kafka/MessageConsumer.java
+++ src/main/java/com/example/myfirstapp/kafka/MessageConsumer.java
@@ -1,11 +1,17 @@
 //package com.example.myfirstapp.kafka;
 //
+//import org.springframework.kafka.annotation.BackOff;
 //import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.annotation.RetryableTopic;
 //import org.springframework.stereotype.Service;
 //
 //@Service
 //public class MessageConsumer {
 //
+//    @RetryableTopic(
+//            attempts = "2",
+//            backOff = @BackOff(delay = 1000)
+//    )
 //    @KafkaListener(
 //            topics = "test-topic",
 //            containerFactory = "cluster1KafkaListenerContainerFactory"
